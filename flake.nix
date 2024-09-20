c01ab58e6281baafdcdb399268d9a77b9f4f5554
Reed <mrnitsuj1@gmail.com>
2024-09-19 23:33:55 -0400

    flake: add nix build output

--- flake.nix
+++ flake.nix
@@ -11,8 +11,26 @@
     devShells.x86_64-linux.default = pkgs.mkShell {
       name = "tx-calculator";
       buildInputs = with pkgs; [
-        ghc
+        (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+          haskellPackages.data-ordlist
+        ]))
       ];
+    };
+    packages.x86_64-linux.tx-calculator = pkgs.stdenv.mkDerivation {
+      name = "tx-calculator";
+      src = ./.;
+      buildInputs = with pkgs; [
+        (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+          haskellPackages.data-ordlist
+        ]))
+      ];
+      buildPhase = ''
+        ghc ./tx.hs -o tx
+      '';
+      installPhase = ''
+        mkdir -p $out/bin
+        cp ./tx $out/bin
+      '';
     };
   };
 }
